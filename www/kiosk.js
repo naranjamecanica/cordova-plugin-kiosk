bc4c86df960fa1ee1f12e162edfaa5290fe0dd9f
Jan Kalina <jkalina@redhat.com>
2016-05-01 20:45:55 +0200

    isInKiosk iOS handling + Apache license

--- www/kiosk.js
+++ www/kiosk.js
@@ -10,6 +10,10 @@ var KioskPlugin = (function() {
         },
 
         isInKiosk: function(callback) {
+            if (/ios|iphone|ipod|ipad/i.test(navigator.userAgent)) {
+                callback(false); // ios not supported - cannot be in kiosk
+                return;
+            }
             exec(function(out) {
                 callback(out === 'true');
             }, function(error) {
