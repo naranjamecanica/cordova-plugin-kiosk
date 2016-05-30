11e208694c0a9245bcfb57c5bce13717dd9ca87a
Julien Ramboz <ramboz@adobe.com>
2016-05-30 08:17:15 +0200

    Fix linting errors

--- kiosk.js
+++ kiosk.js
@@ -1,20 +1,23 @@
-
+/* globals module */
 var exec = require('cordova/exec');
 
-var KioskPlugin = {
+var KioskPlugin = (function() {
+    'use strict';
 
-    exitKiosk: function () {
-        exec(null, null, "KioskPlugin", "exitKiosk", []);
-    },
+    return {
+        exitKiosk: function() {
+            exec(null, null, 'KioskPlugin', 'exitKiosk', []);
+        },
 
-    isInKiosk: function (callback) {
-        exec(function(out){
-            callback(out=="true");
-        }, function(error){
-            throw new Error("KioskPlugin.isInKiosk failed: " + error);
-        }, "KioskPlugin", "isInKiosk", []);
-    }
+        isInKiosk: function(callback) {
+            exec(function(out) {
+                callback(out === 'true');
+            }, function(error) {
+                throw new Error('KioskPlugin.isInKiosk failed: ' + error);
+            }, 'KioskPlugin', 'isInKiosk', []);
+        }
+    };
 
-}
+}());
 
 module.exports = KioskPlugin;
