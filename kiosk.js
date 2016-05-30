2bf5e536ead07d672f3d48395bb9e4be230243ab
Julien Ramboz <ramboz@adobe.com>
2016-05-30 08:13:07 +0200

    Remove window.alert usage and throw a proper error instead

--- kiosk.js
+++ kiosk.js
@@ -2,20 +2,19 @@
 var exec = require('cordova/exec');
 
 var KioskPlugin = {
-    
+
     exitKiosk: function () {
         exec(null, null, "KioskPlugin", "exitKiosk", []);
     },
-    
+
     isInKiosk: function (callback) {
         exec(function(out){
             callback(out=="true");
         }, function(error){
-            alert("KioskPlugin.isInKiosk failed: "+error);
+            throw new Error("KioskPlugin.isInKiosk failed: " + error);
         }, "KioskPlugin", "isInKiosk", []);
     }
-    
+
 }
 
 module.exports = KioskPlugin;
-
